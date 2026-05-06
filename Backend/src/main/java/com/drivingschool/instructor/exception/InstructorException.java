c5933daae2f61087498fdb86daa8e6a044e86c3a
Amaranayaka PDDN <it25101526@my.sliit.lk>
2026-05-06 22:11:01 +0530

    Delete Backend/src/main/java/com/drivingschool/instructor/exception directory

--- Backend/src/main/java/com/drivingschool/instructor/exception/InstructorException.java
+++
@@ -1,7 +0,0 @@
-package com.drivingschool.instructor.exception;
-
-public class InstructorException extends RuntimeException {
-    public InstructorException(String message) {
-        super(message);
-    }
-}
