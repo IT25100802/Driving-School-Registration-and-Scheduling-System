0959a74cdd67bee6378355fdbdbbebe87882be6f
it25103476 <it25103476@my.sliit.lk>
2026-05-11 13:56:32 +0530

    Add TrainingPhase

---
+++ Backend/src/main/java/com/drivingschool/student/entity/TrainingPhase.java
@@ -0,0 +1,10 @@
+package com.drivingschool.student.entity;
+
+public enum TrainingPhase {
+    REGISTRATION,
+    MEDICAL_PENDING,
+    THEORY_TRAINING,
+    PRACTICAL_TRAINING,
+    TRIAL_EXAM,
+    LICENSED
+}
