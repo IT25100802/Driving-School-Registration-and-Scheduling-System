a044294bbdcd06f595535dc5aacbf4dc3382da7d
IT25100802 <IT25100802@my.sliit.lk>
2026-05-13 18:58:15 +0530

    Update AdminRepository

--- Backend/src/main/java/com/drivingschool/admin/repository/AdminRepository.java
+++ Backend/src/main/java/com/drivingschool/admin/repository/AdminRepository.java
@@ -1,5 +1,10 @@
 package com.drivingschool.admin.repository;
 
-public interface AdminRepository  {
+import com.drivingschool.admin.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface AdminRepository extends JpaRepository<Admin, String> {
+    Admin findByUsername(String username);
 
 }
