c2e39d65eb9237c428da8af78d17645cd1c19706
it25103476 <it25103476@my.sliit.lk>
2026-05-14 20:13:40 +0530

    StudentRepository

--- Backend/src/main/java/com/drivingschool/student/repository/StudentRepository.java
+++ Backend/src/main/java/com/drivingschool/student/repository/StudentRepository.java
@@ -4,3 +4,6 @@ import com.drivingschool.student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+}
