8c1e492c1c85ab7a2b92849ce9e68354bfc7434e
it25103476 <it25103476@my.sliit.lk>
2026-05-14 19:43:12 +0530

    StudentDTO

--- Backend/src/main/java/com/drivingschool/student/dto/StudentDTO.java
+++ Backend/src/main/java/com/drivingschool/student/dto/StudentDTO.java
@@ -1,18 +1,11 @@
 package com.drivingschool.student.dto;
 
-import com.drivingschool.student.entity.LicenseCategory;
+import com.drivingschool.payment.entity.LicenseCategory;
+import com.drivingschool.student.entity.TrainingPhase;
 import lombok.Data;
 
 @Data
 public class StudentDTO {
     private String id;
-    private String fullName;
-    private String email;
-    private String phone;
-    private String nic;
-    private String dateOfBirth;
-    private String address;
-    private LicenseCategory licenseCategory;
-    private String enrolledDate;
-    private String status;
+
 }
