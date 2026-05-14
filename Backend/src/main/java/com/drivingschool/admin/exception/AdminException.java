47b13f0df29a03f00c1d56d4a1c2189462b6dd05
IT25100802 <IT25100802@my.sliit.lk>
2026-05-14 10:15:05 +0530

    Update AdminException

--- Backend/src/main/java/com/drivingschool/admin/exception/AdminException.java
+++ Backend/src/main/java/com/drivingschool/admin/exception/AdminException.java
@@ -1,5 +1,8 @@
 package com.drivingschool.admin.exception;
 
 public class AdminException extends RuntimeException {
+    public AdminException(String message) {
+        super(message);
+    }
 
 }
