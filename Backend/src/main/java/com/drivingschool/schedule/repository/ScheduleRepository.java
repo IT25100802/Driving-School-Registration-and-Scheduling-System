0751ceec3390d6d2296672720d52e6f786d53297
IT25101754 <IT25101754@my.sliit.lk>
2026-05-14 23:11:33 +0530

    update ScheduleRepository

--- Backend/src/main/java/com/drivingschool/schedule/repository/ScheduleRepository.java
+++ Backend/src/main/java/com/drivingschool/schedule/repository/ScheduleRepository.java
@@ -1,4 +1,12 @@
 package com.drivingschool.schedule.repository;
 
-public interface ScheduleRepository {
+import com.drivingschool.schedule.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, String> {
+    List<Schedule> findByScheduleDate(String scheduleDate);
 }
