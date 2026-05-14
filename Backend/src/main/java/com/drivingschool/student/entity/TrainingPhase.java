fe50c6bde217fac659ad5fd0c46a245acab597ad
it25103476 <it25103476@my.sliit.lk>
2026-05-14 19:54:15 +0530

    TrainingPhase

--- Backend/src/main/java/com/drivingschool/student/entity/TrainingPhase.java
+++ Backend/src/main/java/com/drivingschool/student/entity/TrainingPhase.java
@@ -1,10 +1,5 @@
 package com.drivingschool.student.entity;
 
 public enum TrainingPhase {
-    REGISTRATION,
-    MEDICAL_PENDING,
-    THEORY_TRAINING,
-    PRACTICAL_TRAINING,
-    TRIAL_EXAM,
-    LICENSED
+ 
 }
