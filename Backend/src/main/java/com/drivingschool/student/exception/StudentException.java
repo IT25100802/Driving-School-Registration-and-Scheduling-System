69a2803415603b6d8bbd5e9588a2d8f1c83b3825
it25103476 <it25103476@my.sliit.lk>
2026-05-14 20:09:24 +0530

    StudentException

--- Backend/src/main/java/com/drivingschool/student/exception/StudentException.java
+++ Backend/src/main/java/com/drivingschool/student/exception/StudentException.java
@@ -1,7 +1,5 @@
 package com.drivingschool.student.exception;
 
 public class StudentException extends RuntimeException {
-    public StudentException(String message) {
-        super(message);
-    }
+   
 }
