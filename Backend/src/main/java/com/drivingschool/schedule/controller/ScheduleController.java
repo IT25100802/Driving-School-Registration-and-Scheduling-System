39e65210259f50749de750c9805e4bea4860014b
IT25101754 <IT25101754@my.sliit.lk>
2026-05-14 12:27:43 +0530

    update ScheduleController

--- Backend/src/main/java/com/drivingschool/schedule/controller/ScheduleController.java
+++ Backend/src/main/java/com/drivingschool/schedule/controller/ScheduleController.java
@@ -1,4 +1,24 @@
 package com.drivingschool.schedule.controller;
 
+import com.drivingschool.schedule.dto.ScheduleDTO;
+import com.drivingschool.schedule.service.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/schedules")
+@CrossOrigin(origins = "*")
+
 public class ScheduleController {
+
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @GetMapping
+    public List<ScheduleDTO> getAllSchedules() {
+        return scheduleService.getAllSchedules();
+    }
 }
