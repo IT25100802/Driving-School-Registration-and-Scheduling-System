195651c71b5fd5bc2d8a717a732ad21a404b8683
it25103476 <it25103476@my.sliit.lk>
2026-05-16 21:31:04 +0530

    update StudentRepository

--- Backend/src/main/java/com/drivingschool/student/repository/StudentRepository.java
+++ Backend/src/main/java/com/drivingschool/student/repository/StudentRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
+    Student findByEmail(String email);
+    @org.springframework.data.jpa.repository.Query(value = "SELECT * FROM student WHERE id LIKE 'S%' ORDER BY CAST(SUBSTRING(id, 2) AS UNSIGNED) DESC LIMIT 1", nativeQuery = true)
+    Student findLastManualId();
 }
