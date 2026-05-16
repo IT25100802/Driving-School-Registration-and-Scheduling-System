fa0980af216581e898d5d708d0658e1567ed0e20
IT25102871 <it25102871@my.sliit.lk>
2026-05-16 23:56:52 +0530

    created CoursePackageRepository

---
+++ Backend/src/main/java/com/drivingschool/payment/repository/CoursePackageRepository.java
@@ -0,0 +1,9 @@
+package com.drivingschool.payment.repository;
+
+import com.drivingschool.payment.entity.CoursePackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoursePackageRepository extends JpaRepository<CoursePackage, String> {
+}
