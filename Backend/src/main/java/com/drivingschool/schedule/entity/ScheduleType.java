6108c075126ecb1060b31f61a0c7211795a66474
IT25101754 <IT25101754@my.sliit.lk>
2026-05-17 17:19:57 +0530

    Revert "Merge branch 'schedule-management' into dev"
    This reverts commit 32d7fa0f21723ea884f3eb0cc3870e52a14e7b90, reversing
    changes made to 8b6035ec376f53a640a20c7031128967990bb941.

--- Backend/src/main/java/com/drivingschool/schedule/entity/ScheduleType.java
+++
@@ -1,8 +0,0 @@
-package com.drivingschool.schedule.entity;
-
-public enum ScheduleType {
-    PRACTICAL_CAR,
-    PRACTICAL_BIKE,
-    THEORY_CLASS,
-    TRIAL_EXAM
-}
--- Backend/src/main/java/com/drivingschool/schedule/exception/ScheduleException.java
+++
@@ -1,7 +0,0 @@
-package com.drivingschool.schedule.exception;
-
-public class ScheduleException extends RuntimeException {
-    public ScheduleException(String message) {
-        super(message);
-    }
-}
